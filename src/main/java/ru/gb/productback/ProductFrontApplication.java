f06241b0befa2a240f288ce5a6312cda5315b79a
olich90 <olich_90@mail.ru>
2022-02-03 00:39:06 +0300

    Initial commit

---
+++ src/main/java/ru/gb/productback/ProductFrontApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.productback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductFrontApplication.class, args);
+	}
+
+}
