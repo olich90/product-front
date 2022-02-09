71200476c06ab992d3b0fca5e5583387afdc2e92
olich90 <olich_90@mail.ru>
2022-02-09 22:15:34 +0300

    hw2_5 PF 2

--- src/main/java/controller/ProductController.java
+++ src/main/java/controller/ProductController.java
@@ -5,19 +5,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Controller
 public class ProductController {
-  private final RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
 
-  public ProductController(RestTemplate restTemplate) {
-    this.restTemplate = restTemplate;
-  }
+    public ProductController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
-  @GetMapping("/front/products")
-  public String getAllProducts(Model model) {
-    return restTemplate.getForObject("http://product-back/getAllProducts", String.class);
-  }
+    @GetMapping("/front/products")
+    public String getAllProducts(Model model) {
+        return restTemplate.getForObject("http://product-back/getAllProducts", String.class);
+    }
 }
--- src/main/java/ru/gb/productback/ProductFrontApplication.java
+++ src/main/java/ru/gb/productback/ProductFrontApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProductFrontApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProductFrontApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProductFrontApplication.class, args);
+    }
 
 }
